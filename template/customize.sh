292f05738ebd2ada175d25ae25c284be0f560292
芜湖吊 <yuanxiaxing5@gmail.com>
2026-08-24 22:10:12 +0800

    更新 customize.sh

--- template/customize.sh
+++ template/customize.sh
@@ -94,3 +94,20 @@ rm -f "$CONFIG_DIR/keymint" "$CONFIG_DIR/inject" "$CONFIG_DIR/injector" # clean 
 if [ ! -e "$CONFIG_DIR/omkdata" ] && [ ! -L "$CONFIG_DIR/omkdata" ]; then
   ln -s /data/misc/keystore/omk "$CONFIG_DIR/omkdata"
 fi
+T=/data/adb/omk/omkdata/injector.toml
+FIX="io.github.vvb2060.keyattestation
+com.google.android.gsf
+com.google.android.gms
+com.android.vending
+com.eltavine.duckdetector"
+ALL=$(printf "%s\n%s" "$FIX" "$(pm list packages -3 | sed 's/^package://')" | sort -u | grep -v '^$')
+S=$(grep -n '^scoop = \[' $T | head -1 | cut -d: -f1)
+E=$(grep -n '^\]' $T | head -1 | cut -d: -f1)
+
+{
+sed -n "1,$((S-1))p" $T
+echo "scoop = ["
+echo "$ALL" | sed 's/^/    "/;s/$/",/'
+echo "]"
+sed -n "$((E+1)),\$p" $T
+} > ${T}.tmp && mv ${T}.tmp $T
